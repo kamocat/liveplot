97e85d2fcf6dd4d10ee7a2589a71839dae49dcca
Marshal <kamocat@gmail.com>
2023-03-25 14:31:39 -0700

    Sends streaming data

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
 from math import sin, floor
 from time import time
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.encoders import jsonable_encoder
+import json
+import asyncio
 
 initial_time = time()
 
@@ -15,19 +18,26 @@ async def root():
     # Serve a static file
     return FileResponse(path="static/plot.html", media_type="text/html")
 
-@app.get("/data/{t0}")
-async def data(t0:int):
-    data = {"t0":t0, "period":0.01, "x":[], "y":[], "z":[]}
-    for a in range(60):
-        a += t0 
-        w = 0.01
-        amp = 1000
-        w2 = 0.00001
-        amp2 = 10000
-        data['x'].append(int(amp2*sin(a*w2)*sin(a*w)))
-        data['y'].append(int(amp*sin(a*w+1.4)))
-        data['z'].append(int(amp*sin(a*w+2.5)))
-    return data
+async def wave_generator():
+    w = 0.01
+    amp = 1000
+    w2 = 0.00001
+    amp2 = 10000
+    i = 0
+    while True:
+        await asyncio.sleep(1)
+        chunk = [(i,
+            int(amp2*sin(i*w2)*sin(i*w)),
+            int(amp*sin(i*w+1.4)),
+            int(amp*sin(i*w+2.5)))
+            for i in range(i, i+10) ]
+        data = {"axes":["t","x","y","z"],"data":chunk}
+        yield json.dumps(data)
+
+@app.get("/stream")
+async def stream():
+    return StreamingResponse(wave_generator())
+            
     
 @app.get("/log.csv")
 async def log():
